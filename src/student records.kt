763befa9b279334a09e030a7e494c49d5fa77610
sharon2719 <otiennosharon@gmail.com>
2021-02-20 22:50:37 +0300

    variables and data types

---
+++ src/student records.kt
@@ -0,0 +1,43 @@
+import java.util.*
+
+fun main() {
+    var name: String="Sharon Akinyi"
+    var age: Byte=23
+    var phoneNumber="+254713767803"
+    var weight: Float=62.5.toFloat()
+    val citizen="Kenyan"
+    var code:Char='S'
+    var hardwork: Boolean=true
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+    println(code)
+    println(hardwork)
+    name="Marie Chantal"
+    age=20
+    phoneNumber="+250781877792"
+    weight=55.5.toFloat()
+    code='M'
+    var hungry: Boolean=false
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+    println(code)
+    println(hungry)
+    val student= arrayOf("Okello OLga Esther",18,"+256782899731",62.1,"Ugandan",'O',"Respectful")
+    println(Arrays.toString(student))
+    var student1= 23
+    var student2=20
+    var student3=18
+    var total=23+20+18
+    println(total)
+
+
+
+
+
+}
